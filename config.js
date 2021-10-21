1de7e2d98177f97588f71cd1eeed27ac7ee9dc9b
Soumili Dey <soumili20au@gmail.com>
2021-10-21 11:33:51 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDOWWMSRbHhTVpIJi3uSn14Yi61mHAE6GU",
+    authDomain: "story-telling-app-b651f.firebaseapp.com",
+    databaseURL: "https://story-telling-app-b651f-default-rtdb.firebaseio.com",
+    projectId: "story-telling-app-b651f",
+    storageBucket: "story-telling-app-b651f.appspot.com",
+    messagingSenderId: "588037187904",
+    appId: "1:588037187904:web:633c432a8545a1b7fc32a5"
+  };
+  
